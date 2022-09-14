1600c256bb9819dd256bdea173311a5016e37e46
andresricca <andresnricca@gmail.com>
2022-09-14 01:13:23 -0300

    TS Páctica Tipos

---
+++ TypeScript/Ejercicio_4.3.ts
@@ -0,0 +1,21 @@
+const numbers: (number | number[])[] = [1, 2, 3, [44, 55], 6, [77, 88], 9, 10];
+
+function flatten<T>(array: (T | T[])[]): T[] {
+    const flattened: T[] = [];
+
+    for (const element of array) {
+        if (Array.isArray(element)) {
+            element; // any[]
+            flattened.push(...element);
+        } else {
+            element; // any
+            flattened.push(element);
+        }
+    }
+
+    return flattened;
+}
+
+const flattenedNumbers: number[] = flatten(numbers);
+
+console.log('[Ejercicio 4.3]', flattenedNumbers);
