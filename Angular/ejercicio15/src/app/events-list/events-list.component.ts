75a8b21f2c56e03e9018ef8995b425438190bd9a
andresricca <andresnricca@gmail.com>
2022-09-21 02:32:54 -0300

    Angular Ej15

---
+++ Angular/ejercicio15/src/app/events-list/events-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../events.service';
+import { IEvent } from '../ievent';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+
+  events!: IEvent[];
+
+  constructor(private eventsService: EventsService) { }
+
+  ngOnInit() {
+    this.events = this.eventsService.getEvents();
+  }
+
+}
